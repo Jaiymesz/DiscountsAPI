54fe454cc8a7381ff9f2e9c2672a93b03aa6b19c
James Parmee Morris <james@gazoop.com>
2021-11-05 22:21:40 +0000

    Initial Base Code

--- src/classes/discountTypes/MultiItem.class.php
+++ src/classes/discountTypes/MultiItem.class.php
@@ -1,13 +1,10 @@
 <?php
 
 class DiscountTypeMultiItem
-{
-    
+{    
+
     public function __construct(){
     
     }
-
-    public function test(){
-        return "MultiItem Tesrt Success";
-    }
+    
 }
