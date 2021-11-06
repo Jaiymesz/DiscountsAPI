6d979297d9852d55c6491153befb9829487d4cfb
James Parmee Morris <james@gazoop.com>
2021-11-06 00:56:26 +0000

    add error handling for orders and ID sanitization

--- src/routes.php
+++ src/routes.php
@@ -4,8 +4,17 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/order/{orderID}', function (Request $request, Response $response, $args) {
     $discounts = new Discounts();
-    $result = $discounts->processCart(json_decode(file_get_contents('https://raw.githubusercontent.com/teamleadercrm/coding-test/master/example-orders/order'.$args['orderID'].'.json'),true));
-	$response = $response->withJson($result, 200);
-	$response = $response->withHeader('Content-Type', 'application/json');
-    return $response;
+
+    $order = @file_get_contents('https://raw.githubusercontent.com/teamleadercrm/coding-test/master/example-orders/order'.preg_replace("/[^0-9\s]/", "",$args['orderID']).'.json');
+    
+    if($order==false){
+        $response = $response->withJson(array("error"=>"Unable to find order ".$args['orderID']), 404);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        return $response;
+    }else{
+        $result = $discounts->processCart(json_decode($order,true));
+        $response = $response->withJson($result, 200);
+        $response = $response->withHeader('Content-Type', 'application/json');
+     } 
+     return $response;    
 });
